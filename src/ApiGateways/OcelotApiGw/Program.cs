841fefab0a34bc66474e2c260b251b2308523e10
ChristianKlecina <djfntt@yahoo.com>
2023-10-15 12:03:27 +0200

    Gateway finished

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,8 +1,10 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+
 
 builder.Host.ConfigureLogging((hostingContext, loggingbuilder) =>
 {
@@ -10,7 +12,14 @@ builder.Host.ConfigureLogging((hostingContext, loggingbuilder) =>
     loggingbuilder.AddConsole();
     loggingbuilder.AddDebug();
 });
-builder.Services.AddOcelot();
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+var app = builder.Build();
+
+
+
 
 app.MapGet("/", () => "Hello World!");
 await app.UseOcelot();
